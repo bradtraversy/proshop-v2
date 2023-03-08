6bd82fa7a07b88d1d9c4ecb3345182cf938b79ba
Brad Traversy <traversymedia@gmail.com>
2023-03-08 09:12:02 -0500

    Product details page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,11 +11,13 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 );
