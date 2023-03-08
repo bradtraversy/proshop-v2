59fcdc796f81c3fc114ebfdf46fe6034eb9c88db
Brad Traversy <traversymedia@gmail.com>
2023-03-08 07:56:55 -0500

    Ratings component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
@@ -10,10 +11,17 @@ const Product = ({ product }) => {
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as='div'>
+          <Card.Title as='div' className='product-title'>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as='div'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
 
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,60 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
+
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className='rating'>
+      <span>
+        {value >= 1 ? (
+          <FaStar />
+        ) : value >= 0.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 2 ? (
+          <FaStar />
+        ) : value >= 1.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 3 ? (
+          <FaStar />
+        ) : value >= 2.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 4 ? (
+          <FaStar />
+        ) : value >= 3.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 5 ? (
+          <FaStar />
+        ) : value >= 4.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span className='rating-text'>{text && text}</span>
+    </div>
+  );
+};
+
+Rating.defaultProps = {
+  color: '#f8e825',
+};
+
+export default Rating;
