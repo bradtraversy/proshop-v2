a83e65b092a923927d745eb42661f16303a5eb3c
Brad Traversy <traversymedia@gmail.com>
2023-03-09 11:40:55 -0500

    Database connect & model data

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import products from './data/products.js';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -17,4 +23,6 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
