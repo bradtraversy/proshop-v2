23d6d10a9b576091a78ee1ab53577af6a5ae5ba0
Brad Traversy <traversymedia@gmail.com>
2023-03-15 09:22:03 -0400

    Checkout handler

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -28,6 +28,10 @@ const CartScreen = () => {
     dispatch(removeFromCart(id));
   };
 
+  const checkoutHandler = () => {
+    navigate('/login?redirect=/shipping');
+  };
+
   return (
     <Row>
       <Col md={8}>
@@ -96,6 +100,7 @@ const CartScreen = () => {
                 type='button'
                 className='btn-block'
                 disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
               >
                 Proceed To Checkout
               </Button>
