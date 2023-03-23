89aaffdc4a6ddab0912edbb2fae8f6fd3bd88f99
Brad Traversy <traversymedia@gmail.com>
2023-03-23 09:44:14 -0400

    Add exp to frontend user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { logout } from './slices/authSlice';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const expirationTime = localStorage.getItem('expirationTime');
+    if (expirationTime) {
+      const currentTime = new Date().getTime();
+      if (currentTime > expirationTime) {
+        dispatch(logout());
+      }
+    }
+  }, [dispatch]);
+
   return (
     <>
       <ToastContainer />
--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -13,10 +13,15 @@ const authSlice = createSlice({
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
       localStorage.setItem('userInfo', JSON.stringify(action.payload));
+
+      const expirationTime = new Date().getTime() + 60 * 60 * 1000; // 1 hour
+      // const expirationTime = new Date().getTime() + 60 * 1000; // 1 minute (for testing)
+      localStorage.setItem('expirationTime', expirationTime);
     },
     logout: (state, action) => {
       state.userInfo = null;
       localStorage.removeItem('userInfo');
+      localStorage.removeItem('expirationTime');
     },
   },
 });
