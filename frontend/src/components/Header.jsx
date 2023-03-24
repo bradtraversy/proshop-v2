f795a9eee04b4a85a7c79ab10ffd3c9d2094ace2
Brad Traversy <traversymedia@gmail.com>
2023-03-24 08:52:54 -0400

    Admin menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -67,6 +67,14 @@ const Header = () => {
                   </Nav.Link>
                 </LinkContainer>
               )}
+
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
+                  <LinkContainer to='/admin/orderlist'>
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
