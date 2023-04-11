de75852b3cf9444f0d0ea689c8500b98e6c9fd43
Brad Traversy <traversymedia@gmail.com>
2023-04-11 17:55:08 -0400

    Updated BASE_URL due to proxy

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,6 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
+// export const BASE_URL =
+//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
+export const BASE_URL = ''; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
