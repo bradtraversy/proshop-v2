8a84ebd6c0f9fc50114cbe54ffeb325d285c6a59
Anthony P <43166263+anthonypz@users.noreply.github.com>
2023-06-12 03:02:07 -0700

    fix(backend): restrict uploads to images
    We have a checkFileType function that only allows image uploads, but it is never used. These changes utilize the function and also responds with a status 400 to the client (instead of a 500) when a non image is uploaded.

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import multer from 'multer';
+
 const router = express.Router();
 
 const storage = multer.diskStorage({
@@ -15,26 +16,33 @@ const storage = multer.diskStorage({
   },
 });
 
-function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/;
+function fileFilter(req, file, cb) {
+  const filetypes = /jpe?g|png|webp/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
+  const mimetype = mimetypes.test(file.mimetype);
 
   if (extname && mimetype) {
-    return cb(null, true);
+    cb(null, true);
   } else {
-    cb({ message: 'Images only!' });
+    cb(new Error('Images only!'), false);
   }
 }
 
-const upload = multer({
-  storage,
-});
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single('image');
 
-router.post('/', upload.single('image'), (req, res) => {
-  res.send({
-    message: 'Image uploaded successfully',
-    image: `/${req.file.path}`,
+router.post('/', (req, res) => {
+  uploadSingleImage(req, res, function (err) {
+    if (err) {
+      res.status(400).send({ message: err.message });
+    }
+
+    res.status(200).send({
+      message: 'Image uploaded successfully',
+      image: `/${req.file.path}`,
+    });
   });
 });
 
