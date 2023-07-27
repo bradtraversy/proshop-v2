074c557aad60ba70a4a8b2eb8d52b82b314eb513
Will Adams <bushblade@gmail.com>
2023-07-27 10:48:26 +0100

    clearer error and commenting

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -90,7 +90,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   // NOTE: here we need to verify the payment was made to PayPal before marking
   // the order as paid
   const { verified, value } = await verifyPayPalPayment(req.body.id);
-  if (!verified) throw new Error('Failed to verify payment');
+  if (!verified) throw new Error('Payment not verified');
 
   // check if this transaction has been used before
   const isNewTransaction = await checkIfNewTransaction(Order, req.body.id);
@@ -99,6 +99,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id);
 
   if (order) {
+    // check the correct amount was paid
     const paidCorrectAmount = order.totalPrice.toString() === value;
     if (!paidCorrectAmount) throw new Error('Incorrect amount paid');
 
