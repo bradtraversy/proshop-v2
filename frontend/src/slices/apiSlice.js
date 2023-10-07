b8c171dd461a5fc18b3c2f84961feafc8ce6359e
Will Adams <bushblade@gmail.com>
2023-10-07 11:26:28 +0100

    fix: (#24) handle 401 responses in frontend

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,10 +1,28 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../constants';
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+import { logout } from './authSlice'; // Import the logout action
+
+// NOTE: code here has changed to handle when our JWT and Cookie expire.
+// We need to customize the baseQuery to be able to intercept any 401 responses
+// and log the user out
+// https://redux-toolkit.js.org/rtk-query/usage/customizing-queries#customizing-queries-with-basequery
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+});
+
+async function baseQueryWithAuth(args, api, extra) {
+  const result = await baseQuery(args, api, extra);
+  // Dispatch the logout action on 401.
+  if (result.error && result.error.status === 401) {
+    api.dispatch(logout());
+  }
+  return result;
+}
 
 export const apiSlice = createApi({
-  baseQuery,
+  baseQuery: baseQueryWithAuth, // Use the customized baseQuery
   tagTypes: ['Product', 'Order', 'User'],
   endpoints: (builder) => ({}),
 });
