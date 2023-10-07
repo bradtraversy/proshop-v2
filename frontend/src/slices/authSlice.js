401ce448b2215a461c34ada446f5d233ade73c61
Will Adams <bushblade@gmail.com>
2023-10-07 11:23:52 +0100

    refactor: remove unused code

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -13,9 +13,6 @@ const authSlice = createSlice({
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
       localStorage.setItem('userInfo', JSON.stringify(action.payload));
-
-      const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 1000; // 30 days
-      localStorage.setItem('expirationTime', expirationTime);
     },
     logout: (state, action) => {
       state.userInfo = null;
