7bf25c57635ba0b2b1c8e1339b3d76fe3e29b055
Will Adams <bushblade@gmail.com>
2023-10-07 11:25:16 +0100

    chore: remove comments

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
 import cartSliceReducer from './slices/cartSlice';
-import authReducer from './slices/authSlice'; // add this line
+import authReducer from './slices/authSlice';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
-    auth: authReducer, // add this line
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
