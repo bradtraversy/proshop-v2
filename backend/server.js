7883579bfca7b4cdf1089e6cdc3099e97dffa55f
Will Adams <bushblade@gmail.com>
2023-10-11 08:56:03 +0100

    indentation

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
   );
 } else {
   const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
