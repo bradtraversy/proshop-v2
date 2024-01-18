e7b228d90762035ab17ee2048e2fe3117c4061c5
Will Adams <bushblade@gmail.com>
2024-01-18 09:20:12 +0000

    clear cookie instead of setting as empty string

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,10 +63,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = (req, res) => {
-  res.cookie('jwt', '', {
-    httpOnly: true,
-    expires: new Date(0),
-  });
+  res.clearCookie('jwt');
   res.status(200).json({ message: 'Logged out successfully' });
 };
 
