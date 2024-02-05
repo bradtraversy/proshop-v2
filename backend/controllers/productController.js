31031acb2d88f9273cf44dfad1aad270c653fd88
iRobot90 <amiandajesse27@gmail.com>
2024-02-05 21:50:15 +0300

    order status update

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
+import { verifyPayPalPayment, checkIfNewTransaction } from '../utils/paypal.js';
 
 // @desc    Fetch all products
 // @route   GET /api/products
