01ebf937fa800ed75ba3293db7f0eca713893a76
iRobot90 <amiandajesse27@gmail.com>
2024-02-27 14:41:22 +0300

    seeder.js

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,22 +1,57 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import users from './data/users.js';
+import products from './data/products.js';
+import User from './models/userModel.js';
+import Product from './models/productModel.js';
+import Order from './models/orderModel.js';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
-const connectDB = async () => {
+connectDB();
+
+const importData = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true, // <-- You can remove this line since it's deprecated
-      useFindAndModify: false,
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+
+    const adminUser = createdUsers[0]._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    await Product.insertMany(sampleProducts);
+
+    console.log('Data Imported!'.green.inverse);
+    process.exit();
   } catch (error) {
-    console.error(`Error: ${error.message}`.red.underline.bold);
+    console.error(`${error}`.red.inverse);
     process.exit(1);
   }
 };
 
-export default connectDB;
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log('Data Destroyed!'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === '-d') {
+  destroyData();
+} else {
+  importData();
+}
